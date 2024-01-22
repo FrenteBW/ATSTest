485a6e9e8183c1015b2af09752e53a16f5ddecd5
An Byungwook <abw2619@icloud.com>
2024-01-22 16:07:27 +0900

    Initial Commit

---
+++ ATSTest/ATSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATSTestApp.swift
+//  ATSTest
+//
+//  Created by 안병욱의 mac on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ATSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
